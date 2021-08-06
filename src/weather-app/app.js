1ade8ff337924aa89efb5a3deaeaec1a1dcc1307
laptop1 <laptop1@gmail.com>
2021-08-05 22:44:51 -0400

    Weather app: Get location from command line

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -1,24 +1,33 @@
 const mapService = require('./mapService.js');
 const weatherService = require('./weatherService.js');
 
-mapService.getLocation('Boston', (error, data) => {
-	if (error) {
-		console.log('Error:', error);
-	} else {
-		console.log('Data: ', data);
+const address = process.argv[2];
 
-		weatherService.getWeather(
-			data.latitude,
-			data.longitude,
-			(error, weatherData) => {
-				if (error) {
-					console.log('Error:', error);
-				} else {
-					console.log(
-						`Weather in ${data.location} is ${weatherData.weather}. It is currently ${weatherData.temperature} degrees outside. It feels like ${weatherData.feelslike} degrees out. There is a ${weatherData.precip}% chance of rain`
-					);
-				}
+if (address) {
+	mapService.getLocation(address, (error, data) => {
+		if (error) {
+			console.log('Error:', error);
+		} else {
+			//console.log('Data: ', data);
+			getWeather(data);
+		}
+	});
+} else {
+	console.error('Please provide an address');
+}
+
+const getWeather = data => {
+	weatherService.getWeather(
+		data.latitude,
+		data.longitude,
+		(error, weatherData) => {
+			if (error) {
+				console.log('Error:', error);
+			} else {
+				console.log(
+					`Weather in ${data.location} is ${weatherData.weather}. It is currently ${weatherData.temperature} degrees outside. It feels like ${weatherData.feelslike} degrees out. There is a ${weatherData.precip}% chance of rain`
+				);
 			}
-		);
-	}
-});
+		}
+	);
+};
