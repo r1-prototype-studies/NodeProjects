deb8ab24075b7945be0a9ae9c7fb9261c482506e
laptop1 <laptop1@gmail.com>
2021-08-13 20:14:37 -0400

    Task Manager App - Implement filtering

--- src/task-manager-app/src/routers/tasks.js
+++ src/task-manager-app/src/routers/tasks.js
@@ -4,10 +4,21 @@ const auth = require('../middlewares/auth');
 
 const router = new express.Router();
 
+//GET /tasks?completed=false/true
 router.get('/tasks', auth, async (req, res) => {
 	try {
+		const match = {};
+		if (req.query.completed) {
+			match.completed = req.query.completed === 'true';
+		}
+
 		//const task = await Task.find();
-		await req.user.populate('tasks').execPopulate();
+		await req.user
+			.populate({
+				path: 'tasks',
+				match,
+			})
+			.execPopulate();
 		res.status(200).send(req.user.tasks);
 	} catch (error) {
 		console.log(error);
